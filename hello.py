70c5428aea3a1e6e998f48fd8abd968535927d44
dayeon3117 <dkang@smith.edu>
2024-02-19 20:20:50 -0500

    Update hello.py
    What I have so far

--- hello.py
+++ hello.py
@@ -1,10 +1,21 @@
 import random
 
-# Define the FSA for 0(01)*
-fsa_001 = {
-    'start': {'0': 'q1'},
-    'q1': {'0': 'q2', 'accept': None},  # 'accept' to potentially end here
-    'q2': {'1': 'q1'},
+# Define the FSA for 0*
+fsa_0_star = {
+    'start': {'0': 'start', 'accept': None},  # Loop back to start on '0', or accept to end
 }
-wejcnwjcnwox
-disdfosjdfo
+
+def generate_word_0_star(fsa, max_length=10):
+    current_state = 'start'
+    word = ''
+    while current_state != 'accept' and len(word) < max_length:
+        transitions = fsa[current_state]
+        next_action = random.choice(list(transitions.keys()))
+        if next_action == 'accept' or len(word) == max_length - 1:
+            break  # End the word
+        word += next_action
+        current_state = transitions[next_action]
+    return word
+
+# Generate a word in the language 0*
+print(generate_word_0_star(fsa_0_star))
